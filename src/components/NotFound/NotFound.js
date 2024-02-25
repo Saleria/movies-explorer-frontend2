488f61474238f6df5a9060ca6ad551adaec52a83
Valeriya K <saleria-v@mail.ru>
2024-02-25 23:26:10 +0300

    fix css

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,23 +1,24 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-function NotFound () {
+function NotFound() {
     const navigate = useNavigate();
-    function returnPage (){
-        navigate(-1, {replace: true});
+    function returnPage() {
+        navigate(-1, { replace: true });
     }
 
-    return(
+    return (
         <section className="not-found">
             <h2 className="not-found__title">
-            404
+                404
             </h2>
             <p className="not-found__subtitle">
-            Страница не найдена
+                Страница не найдена
             </p>
             <button className="not-found__link"
-            onClick={returnPage}
-            >Назад</button>
+                onClick={returnPage}>
+                Назад
+            </button>
         </section>
     );
 }
